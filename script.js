71bc7de39f922ee39875923c5aefa52c9badbe82
n0uremam <84910254+n0uremam@users.noreply.github.com>
2026-02-17 17:01:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -110,7 +110,7 @@
 
   // ===== LIVE UPDATE SETTINGS =====
   var TABLE_REFRESH_MS = 60 * 1000; // tables live refresh
-  var MANIFEST_REFRESH_MS = 3 * 60 * 60 * 1000; // manifest refresh every 3 hours
+  var MANIFEST_REFRESH_MS = 6 * 60 * 60 * 1000; // manifest refresh every 6 hours
 
   // ===== MEDIA PLAYER =====
   var MEDIA_PATH = "media/shared/";
@@ -575,5 +575,6 @@
   debug("Ready ✓");
 
 })();
+
 
 
